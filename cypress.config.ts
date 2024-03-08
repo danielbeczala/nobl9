07a6cbb7475a40f3014ac4c455be10370531002f
Daniel Beczała <danielbeczala@gmail.com>
2024-03-08 20:38:07 +0100

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,14 +1,6 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  env: {
-    login_url: '/',
-    products_url: '/inventory.html',
-    product_page: '/inventory-item.html?id=',
-    standard_user_username: 'standard_user',
-    cart_url: '/cart.html',
-    step_two_checkout_url: '/checkout-step-two.html'
-  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
